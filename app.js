a7dd6d60c6fe432599490e5068fa8b4c9320c88c
GLakshmiPrasanna <gundloorlakshmiprasanna@gmail.com>
2023-05-17 19:18:17 +0530

    bcryot and salt rounds

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
-const md5=require('md5');
+const bcrypt=require('bcrypt');
+const saltRounds=10;
 
 const app=express();
 
@@ -37,24 +38,31 @@ app.get('/register',(req,res)=>{
 
 
 app.post('/register',(req,res)=>{
-    const newUser= new UserModel({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-    newUser.save().then(()=>{
-        res.render('secrets');
-    }).catch((err)=>{
-        console.log(err);
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser= new UserModel({
+            email:req.body.username,
+            password:hash
+        })
+        newUser.save().then(()=>{
+            res.render('secrets');
+        }).catch((err)=>{
+            console.log(err);
+        });
     });
+
 })
 
 app.post('/login',(req,res)=>{
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
 
     UserModel.findOne({email:username}).then((found)=>{
-        if(found && found.password===password){
-            res.render('secrets');
+        if(found){
+            bcrypt.compare(password, found.password, function(err, result) {
+                if(result===true){
+                    res.render('secrets');
+                }
+            });
         }
         else{
             console.log('User not found');
